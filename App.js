2752ea14aa13b7047ade91c690d2a964d8ba3de3
Alif102 <76959764+Alif102@users.noreply.github.com>
2022-04-16 15:12:26 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/react-icons/ai"
+import Resturant from './components/basics/Restaurant';
+
+
+
+
+const App = () => {
+  return (
+    
+    
+    <Resturant />
+
+    
+    
+  );
+};
+
+export default App;
